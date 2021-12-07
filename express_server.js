f72c6f4c45cf10e60dd6d180851effa98fdd2df0
Andrew Bidylo <bidyloandrew@gmail.com>
2021-12-07 01:00:52 +0000

    Added redirect short URLs. Added comments

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.set("view engine", "ejs");
@@ -25,6 +25,7 @@ let urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+// Generate a short URL and get Long URL and push it in DB. After that redirect to a new page with a shortURL.
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   let longURL = req.body.longURL;
@@ -32,24 +33,40 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+// Create a new short URL.
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// Render a page with a new-created shortURL.
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
 
+// After clicking on the short URL: 1) check if this URL exists in DB and then redirect to a long URL.
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  if (longURL === undefined) {
+    res.send("<html><body>URL doesn't exist.</></body></html>\n");
+  } else {
+    res.redirect(longURL);
+  }
+});
+
+// Home page.
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+// Page that shows existing in DB URLs.
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// Hello page.
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
