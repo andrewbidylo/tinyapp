e2181b59bcffa3c17ef6780d39c3606383368842
Andrew Bidylo <bidyloandrew@gmail.com>
2021-12-08 01:47:26 +0000

    Created  login route, login form, button added to the header. Set cookie

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-
+const cookieParser = require('cookie-parser');
 
 app.set("view engine", "ejs");
 
@@ -36,12 +36,13 @@ app.post("/urls", (req, res) => {
 
 // Create a new short URL.
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 // Render a page with a new-created shortURL.
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -53,6 +54,16 @@ app.post('/urls/:id',(req, res)=>{
   urlDatabase[id] = newLongURL;
   res.redirect('/urls');
 });
+
+// Creating a new user
+app.post('/login',(req, res)=>{
+  const username = req.body.username;
+  console.log(username);
+  res.cookie('username', username);
+
+  res.redirect('/urls');
+});
+
 
 // After clicking on the short URL: 1) check if this URL exists in DB and then redirect to a long URL.
 app.get("/u/:shortURL", (req, res) => {
@@ -71,7 +82,7 @@ app.get("/", (req, res) => {
 
 // Page that shows existing in DB URLs.
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
